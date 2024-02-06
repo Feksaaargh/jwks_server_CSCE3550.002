c3717f967972da331b09c32793dd4738759e1802
Feksa <nobody@example.com>
2024-02-05 22:37:53 -0600

    change to rsa for complicatedness (and because the assignment mandates it)

--- jwks-server/tokenmanager.py
+++ jwks-server/tokenmanager.py
@@ -3,8 +3,8 @@ from base64 import urlsafe_b64encode
 from uuid import uuid4
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
-import hmac
 from time import time
+import jwt
 
 # I decided to do this rather than inherit so it pretends it's a simpler object which better suits my needs
 class ExpirableRSAKey:
@@ -17,8 +17,9 @@ class ExpirableRSAKey:
         :param expiration: A float representing seconds since epoch when this token should expire
         :param length: How many bits the RSA key should be (default: 3072)
         """
-        self._key = RSA.generate(length)
-        self.expiration = expiration
+        self._key: RSA.RsaKey = RSA.generate(length)
+        self.expiration: float = expiration
+        self.private: bytes = self._key.export_key()  # for convenience
 
     @property
     def expired(self) -> bool:
@@ -64,7 +65,7 @@ class TokenManager:
             # since expired tokens are only cleaned upon attempted retrieval, this could get bloated...
             del self._tokens[kid]
             return None
-        return f'{{"kty":"oct","alg":"HS256","kid":"{kid}","k":"{urlsafe_b64encode(self._signature).decode("utf-8")}"}}'
+        return f'{{"kty":"RSA","kid":"{kid}","n":"{intToB64(key.n)}","e":"{intToB64(key.e)}"}}'
 
     def makeJWT(self, timeout: float) -> str:
         """
@@ -80,12 +81,5 @@ class TokenManager:
         while kid in self._tokens:
             kid = str(uuid4())
         self._tokens[kid] = key
-        header = urlsafe_b64encode(b'{"alg":"HS256","typ":"JWT","kid":"%s"}'%bytes(kid, 'utf-8'))\
-            .decode('utf-8').rstrip("=")
-        payload = urlsafe_b64encode(b'{"iss":"feksa","exp":"%s"}'%bytes(str(int(key.expiration)), 'utf-8'))\
-            .decode('utf-8').rstrip("=")
-        # sign and return
-        base = f'{header}.{payload}'
-        return base + '.' + urlsafe_b64encode(
-            hmac.new(self._signature, bytes(base, 'utf-8'), 'sha256')
-            .digest()).decode('utf-8').rstrip("=")
+        return jwt.encode({"iss": "feksa", "exp": str(int(key.expiration))}, key.private,
+                          algorithm="RS256", headers={"kid": kid})
--- jwks-server/utils.py
+++ jwks-server/utils.py
@@ -1,9 +1,9 @@
-def intToB64(num: int) -> str:
+def intToB64(num: int, padEven: bool = True) -> str:
     """Encodes an integer into base64. Expects input to be positive."""
     if num < 0: return ""
-    if num == 0: return "A"  # below loop does not work with input of 0
     ret = ""
     while num > 0:
         ret = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"[num%64] + ret
         num = num // 64
+    if padEven and len(ret)%2: return "A"+ret
     return ret
